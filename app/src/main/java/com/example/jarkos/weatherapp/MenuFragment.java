fc027404e9a6c9e275703f3636c02dceb52ea540
Jaroslaw Kostrzewa <180216@edu.p.lodz.pl>
2015-05-01 16:30:16 +0200

    Added ExpandableListAdapter + added it to MenuFragment

--- app/src/main/java/com/example/jarkos/weatherapp/MenuFragment.java
+++ app/src/main/java/com/example/jarkos/weatherapp/MenuFragment.java
@@ -7,12 +7,22 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MenuFragment extends Fragment {
 
     private IFragmentContainer fragmentContainer;
     private Fragment frag;
     private FragmentTransaction fragTransaction;
+
+    private ExpandableListAdapter listAdapter;
+    private ExpandableListView expListView;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> hashListDataChild;
 
     public MenuFragment()
     {}
@@ -27,21 +37,67 @@ public class MenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_menu,container,false);
-
+        expListView = (ExpandableListView) view.findViewById(R.id.expanadableListView);
       // Button btnSimple = (Button) view.findViewById(R.id.btnSimple);
 
-        /*btnSimple.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                frag = new SimpleCalculatorFragment();
-                fragmentContainer.replaceFragment(frag);
+        loadListData();
 
-//                fragTransaction = getFragmentManager().beginTransaction().replace(R.id.container, frag).addToBackStack("simple");
-//                fragTransaction.commit();
+        listAdapter = new ExpandableListAdapter(view.getContext(), listDataHeader, hashListDataChild);
+
+        expListView.setAdapter(listAdapter);
+
+        // Listview group clickListener
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                return false;
             }
-        });*/
+        });
 
         return view;
+    }
+
+    private void loadListData() {
+        listDataHeader = new ArrayList<String>();
+        hashListDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Afryka");
+        listDataHeader.add("Ameryka Płn.");
+        listDataHeader.add("Azja");
+        listDataHeader.add("Europa");
+
+        // Adding child data
+        List<String> africa = new ArrayList<String>();
+        africa.add("C1");
+        africa.add("C2");
+        africa.add("C3");
+        africa.add("C4");
+
+        List<String> northAmerica = new ArrayList<String>();
+        northAmerica.add("C1");
+        northAmerica.add("C2");
+        northAmerica.add("C3");
+        northAmerica.add("C4");
+
+        List<String> asia = new ArrayList<String>();
+        asia.add("C1");
+        asia.add("C4");
+        asia.add("C2");
+        asia.add("C3");
+
+        List<String> europa = new ArrayList<String>();
+        europa.add("C1");
+        europa.add("C4");
+        europa.add("C2");
+        europa.add("C3");
+
+        hashListDataChild.put(listDataHeader.get(0), africa); // Header, Child data
+        hashListDataChild.put(listDataHeader.get(1), northAmerica);
+        hashListDataChild.put(listDataHeader.get(2), asia);
+        hashListDataChild.put(listDataHeader.get(3), europa);
     }
 
 }
