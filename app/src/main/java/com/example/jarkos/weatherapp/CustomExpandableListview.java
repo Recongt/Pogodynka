0aaedcc8537f8aa5c43e66579163fb406f41b9ae
Jaroslaw Kostrzewa <180216@edu.p.lodz.pl>
2015-05-01 21:00:32 +0200

    Added CustomExpandableListview

---
+++ app/src/main/java/com/example/jarkos/weatherapp/CustomExpandableListview.java
@@ -0,0 +1,15 @@
+package com.example.jarkos.weatherapp;
+
+import android.content.Context;
+import android.widget.ExpandableListView;
+
+/**
+ * Created by Jarek on 2015-05-01.
+ */
+public class CustomExpandableListview extends ExpandableListView
+{
+    public CustomExpandableListview(Context context) {
+        super(context);
+    }
+
+}
